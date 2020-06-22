f6742048036b0d173ac6698c8ddb8c3a0b691f1b
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-06-22 02:21:18 +0100

    fix travis issue

---
+++ build/precache-manifest.98af5e5d5ac8ea18bbcfb7a4b5512255.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c063e54360625890ec32a84ab1aabd9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09c88dc2ee2faae19ae5",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "d44d5907936a7f9364c4",
+    "url": "/static/css/main.ca053087.chunk.css"
+  },
+  {
+    "revision": "09c88dc2ee2faae19ae5",
+    "url": "/static/js/2.17737317.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.17737317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d44d5907936a7f9364c4",
+    "url": "/static/js/main.dcaf3325.chunk.js"
+  },
+  {
+    "revision": "053d9e6e7866abf0883e",
+    "url": "/static/js/runtime-main.14ba0b45.js"
+  },
+  {
+    "revision": "100ff8559083d46b30f2ba66be9ca576",
+    "url": "/static/media/amp.100ff855.png"
+  },
+  {
+    "revision": "c43e33d1dc74af76f6c304255e68645e",
+    "url": "/static/media/andela_sponsor.c43e33d1.jpg"
+  },
+  {
+    "revision": "bffb4db4ca113298db4fe9874895c8f0",
+    "url": "/static/media/csb.bffb4db4.png"
+  },
+  {
+    "revision": "f3f45d91abeab09c2dc6f0d5aa1a38e6",
+    "url": "/static/media/event1.f3f45d91.jpg"
+  },
+  {
+    "revision": "d7a13eb03eafbea634893d8b0f494c6d",
+    "url": "/static/media/event2.d7a13eb0.JPG"
+  },
+  {
+    "revision": "0f852ef93a61fe3e6591664510329340",
+    "url": "/static/media/event3.0f852ef9.JPG"
+  },
+  {
+    "revision": "b6c6ee634f046ac6971b4f85b977a221",
+    "url": "/static/media/event4.b6c6ee63.JPG"
+  },
+  {
+    "revision": "028c771a0fa2424528186126b3434038",
+    "url": "/static/media/event5.028c771a.JPG"
+  },
+  {
+    "revision": "6c76b899cd6c18f6134af8308b958815",
+    "url": "/static/media/event6.6c76b899.JPG"
+  },
+  {
+    "revision": "6c285a2fe5122fec8abf59a8f9d0ef24",
+    "url": "/static/media/event7.6c285a2f.JPG"
+  },
+  {
+    "revision": "75b19bc738edef53dc839c768b20fa45",
+    "url": "/static/media/fleek.75b19bc7.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "213a3cc3bd5d9404b79ec5ef12bc9a74",
+    "url": "/static/media/hashnode_sponsor.213a3cc3.svg"
+  },
+  {
+    "revision": "0b2f9cadf2a9ff5f553260e2240718f4",
+    "url": "/static/media/hero.0b2f9cad.png"
+  },
+  {
+    "revision": "59219e1a498a77036a46e134d165aff9",
+    "url": "/static/media/logo.59219e1a.png"
+  },
+  {
+    "revision": "1ce5271b2fc5304e608b70a514f25d28",
+    "url": "/static/media/shodipoayomide.1ce5271b.JPG"
+  },
+  {
+    "revision": "fa17799b538fade5f631f44b981a1f61",
+    "url": "/static/media/thinkster_sponsor.fa17799b.png"
+  },
+  {
+    "revision": "d3c34bafc6c9d5afc058768b95150aa1",
+    "url": "/static/media/unstack-locate.d3c34baf.svg"
+  },
+  {
+    "revision": "0a5a2217e524cd0802825527bb96ce64",
+    "url": "/static/media/vanhack_sponsor.0a5a2217.png"
+  },
+  {
+    "revision": "f02ff37bd66600e9f063bba01e24b654",
+    "url": "/static/media/ztp.f02ff37b.jpg"
+  }
+]);
