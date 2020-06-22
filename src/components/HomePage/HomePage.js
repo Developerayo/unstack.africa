ab05553ec9e8bb486487259b0aa606e085d86d2c
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-06-22 01:03:57 +0100

    in progress...

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -15,16 +15,17 @@ import event6 from "../../assets/img/event6.JPG"
 import event7 from "../../assets/img/event7.JPG"
 import replace from "../../assets/img/event1.jpg"
 import cloudinary from "../../assets/img/cloudinary_sponsor.png"
-import codesandbox from "../../assets/img/codesandbox_sponsor.png"
+import codesandbox from "../../assets/img/csb.png"
 import egghead from "../../assets/img/egghead_sponsor.jpg"
 import andela from "../../assets/img/andela_sponsor.jpg"
 import amp from "../../assets/img/amp_sponsor.png"
-import fleek from "../../assets/img/fleek_sponsor.png"
+import fleek from "../../assets/img/fleek.png"
 import vanhack from "../../assets/img/vanhack_sponsor.png"
-import gatsby from "../../assets/img/gatsby_sponsor.png"
+import gatsby from "../../assets/img/gatsby.png"
 import paystack from "../../assets/img/paystack_sponsor.png"
 import thinkster from "../../assets/img/thinkster_sponsor.png"
 import hashnode from "../../assets/img/hashnode_sponsor.svg"
+import '../../../node_modules/font-awesome/css/font-awesome.min.css'; 
 
 const HomePage = props => (
   <Fragment>
@@ -1395,60 +1396,99 @@ const HomePage = props => (
               </p>
             </div>
           </div>
-      
-          <div className="row">
-            <div className="sponsor-block">
+      <div style={{display: "flex"}}>
+      <svg style={{color: "#e49205"}} xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="grip-lines-vertical" className="svg-inline--fa fa-grip-lines-vertical fa-w-8" width="20px" role="img" viewBox="0 0 256 512"><path fill="currentColor" d="M96 496V16c0-8.8-7.2-16-16-16H48c-8.8 0-16 7.2-16 16v480c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16zm128 0V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v480c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16z"/></svg>
+         <h2 style={{alignSelf: "center", marginLeft: "10px", fontSize: "2em", fontFamily: "sans-serif"}}>Platinum Sponsors</h2>
+      </div>
+
+          
+          <div className="row mt-50 mb-50">
+            <div className="sponsor-block col-md-4 col-sm-2 mx-5">
                 <a href="https://cloudinary.com" target="_blank" rel="noopener noreferrer">
-                  <img src={cloudinary} alt="" />
+                  <img src={cloudinary} alt="" className="move-right"/>
                 </a>
               </div>
-              <div className="col-md-2 col-sm-2">
-                <a href="https://codesandbox.io" target="_blank" rel="noopener noreferrer">
-                  <img src={hashnode} alt="" />
+             
+              </div>
+        
+
+              <div style={{display: "flex"}}>
+      <svg style={{color: "#e49205"}} xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="grip-lines-vertical" className="svg-inline--fa fa-grip-lines-vertical fa-w-8" width="20px" role="img" viewBox="0 0 256 512"><path fill="currentColor" d="M96 496V16c0-8.8-7.2-16-16-16H48c-8.8 0-16 7.2-16 16v480c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16zm128 0V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v480c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16z"/></svg>
+         <h2 style={{alignSelf: "center", marginLeft: "10px", fontSize: "2em", fontFamily: "sans-serif"}}>Silver Sponsors</h2>
+      </div>
+          <div className="row mt-50 mb-50">
+            <div className="sponsor-block col-md-3 col-sm-2">
+                <a href="https://codesandbox.com" target="_blank" rel="noopener noreferrer">
+                <img src={fleek} alt="" className="move-right" style={{width: "150px"}}/>
                 </a>
               </div>
-              <div className="col-md-2 col-sm-2">
+              <div className="sponsor-block col-md-3 col-sm-2">
+                <a href="https://fleek.co" target="_blank" rel="noopener noreferrer">
+                  <img src={codesandbox} alt="" className="move-right" style={{width: "150px"}}/>
+                </a>
+              </div>
+              <div className="col-md-3 col-sm-2">
                 <a href="https://egghead.io" target="_blank" rel="noopener noreferrer">
-                  <img src={thinkster} alt="" />
+                  <img src={thinkster} alt="" className="move-right"/>
                 </a>
               </div>
           </div>
 
-          <div className="row">
-            <div className="sponsor-block">
-                <a href="https://cloudinary.com" target="_blank" rel="noopener noreferrer">
-                  <img src={cloudinary} alt="" />
-                </a>
-              </div>
-              <div className="col-md-2 col-sm-2">
-                <a href="https://codesandbox.io" target="_blank" rel="noopener noreferrer">
-                  <img src={hashnode} alt="" />
-                </a>
-              </div>
-              <div className="col-md-2 col-sm-2">
-                <a href="https://egghead.io" target="_blank" rel="noopener noreferrer">
-                  <img src={thinkster} alt="" />
-                </a>
-              </div>
-          </div>
+          <div style={{display: "flex"}}>
+      <svg style={{color: "#e49205"}} xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="grip-lines-vertical" className="svg-inline--fa fa-grip-lines-vertical fa-w-8" width="20px" role="img" viewBox="0 0 256 512"><path fill="currentColor" d="M96 496V16c0-8.8-7.2-16-16-16H48c-8.8 0-16 7.2-16 16v480c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16zm128 0V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v480c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16z"/></svg>
+         <h2 style={{alignSelf: "center", marginLeft: "10px", fontSize: "2em", fontFamily: "sans-serif"}}>Bronze Sponsors</h2>
+      </div>
 
-          <div className="row">
-            <div className="sponsor-block">
-                <a href="https://cloudinary.com" target="_blank" rel="noopener noreferrer">
-                  <img src={cloudinary} alt="" />
+          
+          <div className="row mt-50 mb-20">
+            <div className="sponsor-block col-md-2 col-sm-2">
+                <a href="https://paystack.co" target="_blank" rel="noopener noreferrer">
+                  <img src={paystack} alt="" className="move-right"/>
+                </a>
+              </div>
+              <div className="sponsor-block col-md-2 col-sm-2">
+                <a href="https://hashnode.com" target="_blank" rel="noopener noreferrer">
+                  <img src={hashnode} alt="" className="move-right"/>
                 </a>
               </div>
               <div className="col-md-2 col-sm-2">
-                <a href="https://codesandbox.io" target="_blank" rel="noopener noreferrer">
-                  <img src={hashnode} alt="" />
+                <a href="https://gatsby.com" target="_blank" rel="noopener noreferrer">
+                  <img src={gatsby} alt="" className="move-right" style={{width: "125px"}}/>
+                </a>
+              </div>
+              <div className="sponsor-block col-md-2 col-sm-2">
+                <a href="https://cloudinary.com" target="_blank" rel="noopener noreferrer">
+                  <img src={cloudinary} alt="" className="move-right"/>
+                </a>
+              </div>
+              <div className="sponsor-block col-md-2 col-sm-2">
+                <a href="https://cloudinary.com" target="_blank" rel="noopener noreferrer">
+                  <img src={cloudinary} alt="" className="move-right"/>
                 </a>
               </div>
               <div className="col-md-2 col-sm-2">
                 <a href="https://egghead.io" target="_blank" rel="noopener noreferrer">
-                  <img src={thinkster} alt="" />
+                  <img src={thinkster} alt="" className="move-right"/>
                 </a>
               </div>
-          </div>
+              </div>
+          <div className="row">
+              <div className="col-md-2 col-sm-2">
+                <a href="https://egghead.io" target="_blank" rel="noopener noreferrer">
+                  <img src={thinkster} alt="" className="move-right"/>
+                </a>
+              </div>
+              <div className="col-md-2 col-sm-2">
+                <a href="https://egghead.io" target="_blank" rel="noopener noreferrer">
+                  <img src={thinkster} alt="" className="move-right"/>
+                </a>
+              </div>
+              <div className="col-md-2 col-sm-2">
+                <a href="https://egghead.io" target="_blank" rel="noopener noreferrer">
+                  <img src={thinkster} alt="" className="move-right"/>
+                </a>
+              </div>
+              </div>
       
         </div>
             {/* <div className="sponsor-block">
@@ -1456,6 +1496,11 @@ const HomePage = props => (
                   <img src={cloudinary} alt="" />
                 </a>
               </div>
+                <div className="col-md-2 col-sm-2">
+                <a href="https://paystack.com" target="_blank" rel="noopener noreferrer">
+                  <img src={paystack} alt="" />
+                </a>
+              </div>
               <div className="col-md-2 col-sm-2">
                 <a href="https://codesandbox.io" target="_blank" rel="noopener noreferrer">
                   <img src={hashnode} alt="" />
@@ -1467,8 +1512,8 @@ const HomePage = props => (
                 </a>
               </div>
               <div className="col-md-2 col-sm-2">
-                <a href="https://fleek.co" target="_blank" rel="noopener noreferrer">
-                  <img src={paystack} alt="" />
+                <a href="https://codesandbox.io" target="_blank" rel="noopener noreferrer">
+                  <img src={hashnode} alt="" />
                 </a>
               </div>
               <div className="col-md-2 col-sm-2">
