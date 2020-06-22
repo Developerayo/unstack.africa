d6531056a9bc7a0ac22df420ef2fe7e5fa724a4e
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-06-22 01:40:04 +0100

    more fixes

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -16,9 +16,9 @@ import event7 from "../../assets/img/event7.JPG"
 import replace from "../../assets/img/event1.jpg"
 import cloudinary from "../../assets/img/cloudinary_sponsor.png"
 import codesandbox from "../../assets/img/csb.png"
-import egghead from "../../assets/img/egghead_sponsor.jpg"
+import egghead from "../../assets/img/egghead.jpg"
 import andela from "../../assets/img/andela_sponsor.jpg"
-import amp from "../../assets/img/amp_sponsor.png"
+import amp from "../../assets/img/amp.png"
 import fleek from "../../assets/img/fleek.png"
 import vanhack from "../../assets/img/vanhack_sponsor.png"
 import gatsby from "../../assets/img/gatsby.png"
@@ -1396,13 +1396,13 @@ const HomePage = props => (
               </p>
             </div>
           </div>
-      <div style={{display: "flex"}}>
+      <div className="platinum-header" style={{display: "flex"}}>
       <svg style={{color: "#e49205"}} xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="grip-lines-vertical" className="svg-inline--fa fa-grip-lines-vertical fa-w-8" width="20px" role="img" viewBox="0 0 256 512"><path fill="currentColor" d="M96 496V16c0-8.8-7.2-16-16-16H48c-8.8 0-16 7.2-16 16v480c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16zm128 0V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v480c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16z"/></svg>
-         <h2 style={{alignSelf: "center", marginLeft: "10px", fontSize: "2em", fontFamily: "sans-serif"}}>Platinum Sponsors</h2>
+         <h2 style={{alignSelf: "center", marginLeft: "10px", fontSize: "2em", fontFamily: "sans-serif"}}>Platinum</h2>
       </div>
 
           
-          <div className="row mt-50 mb-50">
+          <div className="row mt-50 mb-50 platinum">
             <div className="sponsor-block col-md-4 col-sm-2 mx-5">
                 <a href="https://cloudinary.com" target="_blank" rel="noopener noreferrer">
                   <img src={cloudinary} alt="" className="move-right"/>
@@ -1414,29 +1414,29 @@ const HomePage = props => (
 
               <div style={{display: "flex"}}>
       <svg style={{color: "#e49205"}} xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="grip-lines-vertical" className="svg-inline--fa fa-grip-lines-vertical fa-w-8" width="20px" role="img" viewBox="0 0 256 512"><path fill="currentColor" d="M96 496V16c0-8.8-7.2-16-16-16H48c-8.8 0-16 7.2-16 16v480c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16zm128 0V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v480c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16z"/></svg>
-         <h2 style={{alignSelf: "center", marginLeft: "10px", fontSize: "2em", fontFamily: "sans-serif"}}>Silver Sponsors</h2>
+         <h2 style={{alignSelf: "center", marginLeft: "10px", fontSize: "2em", fontFamily: "sans-serif"}}>Silver</h2>
       </div>
           <div className="row mt-50 mb-50">
-            <div className="sponsor-block col-md-3 col-sm-2">
+            <div className="sponsor-block col-md-2 col-sm-2">
                 <a href="https://codesandbox.com" target="_blank" rel="noopener noreferrer">
                 <img src={fleek} alt="" className="move-right" style={{width: "150px"}}/>
                 </a>
               </div>
               <div className="sponsor-block col-md-3 col-sm-2">
                 <a href="https://fleek.co" target="_blank" rel="noopener noreferrer">
-                  <img src={codesandbox} alt="" className="move-right" style={{width: "150px"}}/>
+                  <img src={codesandbox} alt="" className="move-right" style={{width: "200px"}}/>
                 </a>
               </div>
               <div className="col-md-3 col-sm-2">
                 <a href="https://egghead.io" target="_blank" rel="noopener noreferrer">
-                  <img src={thinkster} alt="" className="move-right"/>
+                  <img src={amp} alt="" className="move-right" style={{width: "130px"}}/>
                 </a>
               </div>
           </div>
 
           <div style={{display: "flex"}}>
       <svg style={{color: "#e49205"}} xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="grip-lines-vertical" className="svg-inline--fa fa-grip-lines-vertical fa-w-8" width="20px" role="img" viewBox="0 0 256 512"><path fill="currentColor" d="M96 496V16c0-8.8-7.2-16-16-16H48c-8.8 0-16 7.2-16 16v480c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16zm128 0V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v480c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16z"/></svg>
-         <h2 style={{alignSelf: "center", marginLeft: "10px", fontSize: "2em", fontFamily: "sans-serif"}}>Bronze Sponsors</h2>
+         <h2 style={{alignSelf: "center", marginLeft: "10px", fontSize: "2em", fontFamily: "sans-serif"}}>Bronze</h2>
       </div>
 
           
@@ -1457,13 +1457,13 @@ const HomePage = props => (
                 </a>
               </div>
               <div className="sponsor-block col-md-2 col-sm-2">
-                <a href="https://cloudinary.com" target="_blank" rel="noopener noreferrer">
-                  <img src={cloudinary} alt="" className="move-right"/>
+                <a href="https://andela.com" target="_blank" rel="noopener noreferrer">
+                  <img src={andela} alt="" className="move-right" style={{width: "110px"}}/>
                 </a>
               </div>
               <div className="sponsor-block col-md-2 col-sm-2">
-                <a href="https://cloudinary.com" target="_blank" rel="noopener noreferrer">
-                  <img src={cloudinary} alt="" className="move-right"/>
+                <a href="https://egghead.io" target="_blank" rel="noopener noreferrer">
+                  <img src={egghead} alt="" className="move-right" style={{width: "110px"}}/>
                 </a>
               </div>
               <div className="col-md-2 col-sm-2">
