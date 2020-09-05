bc41876b0190e1ab482c808429a041ca4d95b546
Ademola Adegbuyi <ooade96@gmail.com>
2020-09-05 10:57:41 +0100

    Change jobTitle to bio

--- src/components/HomePage/Speakers.js
+++ src/components/HomePage/Speakers.js
@@ -4,7 +4,7 @@ import shodipoayomide from "../../assets/img/speakers/shodipoayomide.JPG"
 const speakers = [
   {
     name: "Cassidy Williams",
-    jobTitle: "Principal Developer Experience Engineer, Netlify",
+    bio: "Principal Developer Experience Engineer, Netlify",
     imageURL:
       "https://pbs.twimg.com/profile_images/1292888896938729473/tDmJ1K6m_400x400.jpg",
     links: {
@@ -15,7 +15,7 @@ const speakers = [
   },
   {
     name: "Kent C. Dodds",
-    jobTitle: "Technical Trainer",
+    bio: "Technical Trainer",
     imageURL:
       "https://pbs.twimg.com/profile_images/759557613445001216/6M2E1l4q.jpg",
     links: {
@@ -26,7 +26,7 @@ const speakers = [
   },
   {
     name: "Max Stoiber",
-    jobTitle: "Staff Software Engineer, Gatsby",
+    bio: "Staff Software Engineer, Gatsby",
     imageURL:
       "https://media.graphcms.com/output=format:jpg/resize=fit:crop,height:500,width:500/2jqu4t6TSlKDcqRFP6zT",
     links: {
@@ -37,7 +37,7 @@ const speakers = [
   },
   {
     name: "Christian Nwamba",
-    jobTitle: "Senior Cloud Developer Advocate, Microsoft",
+    bio: "Senior Cloud Developer Advocate, Microsoft",
     imageURL:
       "https://pbs.twimg.com/profile_images/1105185322084847623/EPx48HPQ_400x400.jpg",
     links: {
@@ -49,7 +49,7 @@ const speakers = [
   },
   {
     name: "Shodipo Ayomide",
-    jobTitle: "Founder, unStack Africa & Senior Developer Advocate",
+    bio: "Founder, unStack Africa & Senior Developer Advocate",
     imageURL: shodipoayomide,
     links: {
       github: "https://github.com/developerayo",
@@ -59,7 +59,7 @@ const speakers = [
   },
   {
     name: "Kingdom Orjiewuru",
-    jobTitle: "Co-Founder unStack Africa & Software Engineer, Aula",
+    bio: "Co-Founder unStack Africa & Software Engineer, Aula",
     imageURL:
       "https://avatars2.githubusercontent.com/u/26261917?s=400&u=6a307e226cd2aef7f38968644047a8e80e336a4f&v=4",
     links: {
@@ -71,7 +71,7 @@ const speakers = [
 ]
 
 const Speakers = () => {
-  return speakers.map(({ name, imageURL, jobTitle, links }) => {
+  return speakers.map(({ name, imageURL, bio, links }) => {
     return (
       <div className="col-sm-3 pd-0" key={name}>
         <div className="block-speaker">
@@ -82,7 +82,7 @@ const Speakers = () => {
             <div className="block-info-2">
               <p>
                 <strong>{name}</strong>
-                <span>{jobTitle}</span>
+                <span>{bio}</span>
               </p>
               <ul className="block-social">
                 {Object.keys(links).map((linkKey) => {
