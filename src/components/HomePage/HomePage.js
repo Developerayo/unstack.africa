c03890033a46fb7deee2de61e1fbc34972a601e3
Ademola Adegbuyi <ooade96@gmail.com>
2020-09-05 11:56:00 +0100

    Modularize location section

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,13 +4,13 @@ import Team from "./Team"
 import Gallery from "./Gallery"
 import Speakers from "./Speakers"
 import FAQ from "./FAQ"
+import Location from "./Location"
 
 import Header from "../common/Header"
 import Footer from "../common/Footer"
 
 import "../../../node_modules/font-awesome/css/font-awesome.min.css"
 import hero from "../../assets/img/hero.png"
-import unstacklocate from "../../assets/img/unstack-locate.svg"
 // import replace from "../../assets/img/event1.jpg"
 // import cloudinary from "../../assets/img/cloudinary_sponsor.png"
 // import codesandbox from "../../assets/img/csb.png"
@@ -602,61 +602,7 @@ const HomePage = (props) => (
         </div>
       </section> */}
 
-      <section id="location" className="location bg-dark">
-        <div className="container">
-          <div className="row locate">
-            <div className="col-sm-12 col-md-6">
-              <img
-                loading="lazy"
-                src={unstacklocate}
-                alt="unstack"
-                style={{ width: "320px" }}
-              />
-            </div>
-            <div className="col-sm-12 col-md-6">
-              <h1 className="title mt-40">
-                Reach out to the developer community{" "}
-              </h1>
-
-              <div className="block-info-3">
-                <p>
-                  Africa | Lagos, Nigeria = HQ
-                  <br />
-                  <br />
-                  <i className="icon-twitter"></i>{" "}
-                  <a
-                    href="https://twitter.com/unstackafrica"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{ color: "white" }}
-                  >
-                    Twitter
-                  </a>{" "}
-                  <br />
-                  <i className="icon-slack"></i>
-                  <a
-                    href="https://join.slack.com/t/unstackdev/shared_invite/zt-9bowaas5-jhDbuts0FEJVKOaNz~1C4g"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{ color: "white" }}
-                  >
-                    Slack
-                  </a>{" "}
-                  <br />
-                  Email :{" "}
-                  <a
-                    href="mailto:unstackdev@gmail.com"
-                    style={{ color: "white" }}
-                  >
-                    unstackdev@gmail.com
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
+      <Location />
       <FAQ />
       <Gallery />
       <Footer />
