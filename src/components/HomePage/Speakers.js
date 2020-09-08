43ad7df5355cd6f4fbf04af81408cbd3b725723f
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-09-08 12:19:44 +0100

    Setup 5 speakers

--- src/components/HomePage/Speakers.js
+++ src/components/HomePage/Speakers.js
@@ -1,52 +1,63 @@
 import React from "react"
 // import shodipoayomide from "../../assets/img/speakers/shodipoayomide.JPG"
 
-// const speakers = [
-//   {
-//     name: "Cassidy Williams",
-//     bio: "Principal Developer Experience Engineer, Netlify",
-//     imageURL:
-//       "https://pbs.twimg.com/profile_images/1292888896938729473/tDmJ1K6m_400x400.jpg",
-//     links: {
-//       github: "https://github.com/cassidoo",
-//       twitter: "https://twitter.com/cassidoo",
-//       linkedin: "https://www.linkedin.com/in/cassidoo",
-//     },
-//   },
-//   {
-//     name: "Kent C. Dodds",
-//     bio: "Technical Trainer",
-//     imageURL:
-//       "https://pbs.twimg.com/profile_images/759557613445001216/6M2E1l4q.jpg",
-//     links: {
-//       github: "https://github.com/kentcdodds",
-//       twitter: "https://twitter.com/kentcdodds",
-//       linkedin: "https://www.linkedin.com/in/kentcdodds",
-//     },
-//   },
-//   {
-//     name: "Max Stoiber",
-//     bio: "Staff Software Engineer, Gatsby",
-//     imageURL:
-//       "https://media.graphcms.com/output=format:jpg/resize=fit:crop,height:500,width:500/2jqu4t6TSlKDcqRFP6zT",
-//     links: {
-//       github: "https://github.com/mxstbr",
-//       twitter: "https://twitter.com/mxstbr",
-//       linkedin: "https://www.linkedin.com/in/mxstbr",
-//     },
-//   },
-//   {
-//     name: "Christian Nwamba",
-//     bio: "Senior Cloud Developer Advocate, Microsoft",
-//     imageURL:
-//       "https://pbs.twimg.com/profile_images/1105185322084847623/EPx48HPQ_400x400.jpg",
-//     links: {
-//       github: "https://github.com/christiannwamba",
-//       twitter: "https://twitter.com/codebeast",
-//       linkedin:
-//         "https://www.linkedin.com/in/christian-nwamba-032b04b2/?originalSubdomain=ng",
-//     },
-//   },
+const speakers = [
+  {
+    name: "Prosper Otemuyiwa",
+    bio: "Co-Founder & CTO, Eden Life",
+    imageURL:
+      "https://miro.medium.com/max/500/1*tv9pIQPhwumDnYBfCoapYg.jpeg",
+    links: {
+      github: "https://github.com/unicodeveloper",
+      twitter: "https://twitter.com/unicodeveloper",
+      linkedin: "https://ng.linkedin.com/in/prosperotemuyiwa",
+    },
+  },
+  {
+    name: "James Q. Quick",
+    bio: "Senior Developer Advocacte, Auth0",
+    imageURL:
+      "https://yt3.ggpht.com/a/AATXAJw6goL85GOINrpbAgISieyTKQWx0d8mVLY4Eanb6g=s900-c-k-c0xffffffff-no-rj-mo",
+    links: {
+      github: "https://github.com/jamesqquick",
+      twitter: "https://twitter.com/jamesqquick",
+      linkedin: "https://www.linkedin.com/in/jamesqquick",
+    },
+  },
+  {
+  name: "Tessa Mero",
+  bio: "Developer Advocacte, Cloudinary",
+  imageURL:
+    "https://pbs.twimg.com/profile_images/1256720658798534657/22pbZTRW.jpg",
+  links: {
+    github: "https://github.com/tessamero",
+    twitter: "https://twitter.com/tessamero",
+    linkedin: "https://www.linkedin.com/in/tessamero",
+  },
+},
+  {
+    name: "Segun Adebayo",
+    bio: "Creator, Chakra UI",
+    imageURL:
+      "https://pbs.twimg.com/profile_images/1169353373012897802/skPUWd6e_400x400.jpg",
+    links: {
+      github: "https://github.com/mxstbr",
+      twitter: "https://twitter.com/mxstbr",
+      linkedin: "https://www.linkedin.com/in/mxstbr",
+    },
+  },
+  {
+    name: "Domitrius Clark",
+    bio: "Advocate Engineer, Cloudinary",
+    imageURL:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQFMkMlJneUeeQ/profile-displayphoto-shrink_800_800/0?e=1605139200&v=beta&t=xe6RM3zbxA119i4B-hg0uMGjO4i6IE8umKOpIFFGtg4",
+    links: {
+      github: "https://github.com/domitriusclark",
+      twitter: "https://twitter.com/domitriusclark",
+      linkedin:
+        "https://www.linkedin.com/in/domitriusanthony/",
+    },
+  },
 //   {
 //     name: "Shodipo Ayomide",
 //     bio: "Founder, unStack Africa & Senior Developer Advocate",
@@ -68,7 +79,7 @@ import React from "react"
 //       linkedin: "https://ng.linkedin.com/in/kingdomorjiewuru",
 //     },
 //   },
-// ]
+]
 
 const Speakers = () => {
   return (
@@ -80,45 +91,7 @@ const Speakers = () => {
           </div>
         </div>
       </div>
-
-      <div className="block-content row">
-        <div className="col-sm-3 pd-0">
-          <div className="block-speaker">
-            <div className="block-img overlay soft">
-              <div className="background-img">
-                <img
-                  loading="lazy"
-                  src="https://cdn.vox-cdn.com/thumbor/IDuU1a0FYBrTb_X0tt5gCyTeALU=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/10164247/BlackPanther596d2f04d1540_2040.jpg"
-                  alt=""
-                />
-              </div>
-              <div className="block-info-2">
-                <p>
-                  <strong>Coming Soon</strong>
-                  <span>WAKANDA FOREVER </span>
-                </p>
-                <ul className="block-social">
-                  <li>
-                    <a href="/" target="_blank" rel="noopener noreferrer">
-                      <i className="fa fa-github"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/" target="_blank" rel="noopener noreferrer">
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/" target="_blank" rel="noopener noreferrer">
-                      <i className="fa fa-linkedin"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* {speakers.map(({ name, imageURL, bio, links }) => {
+       {speakers.map(({ name, imageURL, bio, links }) => {
           return (
             <div className="col-sm-3 pd-0" key={name}>
               <div className="block-speaker">
@@ -153,8 +126,7 @@ const Speakers = () => {
               </div>
             </div>
           )
-        })} */}
-      </div>
+        })} 
     </section>
   )
 }
