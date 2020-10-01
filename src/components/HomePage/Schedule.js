94a48171879a1e956de404f3eae6c202a67af394
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-10-01 21:24:41 +0100

    fix amanda time

--- src/components/HomePage/Schedule.js
+++ src/components/HomePage/Schedule.js
@@ -390,7 +390,7 @@ const Schedule = () => {
                   
                         </div>
                         <div className="block-detail">
-                          <span className="time">10:10 - 10:30</span>
+                          <span className="time">10:15 - 10:35</span>
                           <span className="topic">TBA<h6 style={{color: "#E49205", fontWeight:"bold"}}>Keynote</h6></span>
                             <span className="speaker">
                               {" "}
