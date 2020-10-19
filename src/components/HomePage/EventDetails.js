ebe7e49dc9322324396c6d1094f83000a357b118
Olumide Okedusi <odokedusi@sidmach.com>
2020-10-19 15:27:52 +0100

    Comment unused variables

--- src/components/HomePage/EventDetails.js
+++ src/components/HomePage/EventDetails.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Link} from "react-router-dom"
+// import {Link} from "react-router-dom"
 const EventDetails = () => {
   return (
     <section id="about" className="about pt-100 pb-100 brd-bottom">
--- src/components/HomePage/Main.js
+++ src/components/HomePage/Main.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Link} from "react-router-dom"
+// import {Link} from "react-router-dom"
 
 import Header from "../common/Header"
 import hero from "../../assets/img/hero.png"
