8a76751357a49feb2d2155f930bc253d3ce1ad69
Shodipo Ayomide <shodipovi@gmail.com>
2021-10-02 16:36:13 +0100

    Add GH Speaker

--- src/components/HomePage/Speakers.js
+++ src/components/HomePage/Speakers.js
@@ -67,6 +67,20 @@ const speakers = [
     },
   },
   {
+    name: "Priscila Heller",
+    keynote: "GITHUB ACTIONS",
+    bio: "Senior Manager, Premium Support at GitHub",
+    imageURL:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQEebuUcQnEsFQ/profile-displayphoto-shrink_800_800/0/1585346461026?e=1638403200&v=beta&t=SHJn7m879ogqnyV0KZRoUs1NbLZmXaC2DZOggjAMk8s",
+    links: {
+      github: "https://github.com",
+      twitter: "https://twitter.com/Samson_Goddy",
+      linkedin:
+        "https://www.linkedin.com/in/priscila-heller/",
+    },
+  },
+
+  {
     name: "Kingdom Orjiewuru",
     keynote: "GRAPHQL",
     bio: "Co-Founder at unStack & Senior Software Engineer, Lounge HQ",
