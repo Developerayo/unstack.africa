179f0a5ff22f499497b105ee4f62ef2f31fdc7a0
Shodipo Ayomide <shodipovi@gmail.com>
2021-10-08 17:44:29 +0100

    View Speakers

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -5,11 +5,11 @@ import FAQ from "./FAQ"
 import Team from "./Team"
 import Main from "./Main"
 import Gallery from "./Gallery"
-// import Speakers from "./Speakers"
-// import Schedule from "./Schedule"
+import Speakers from "./Speakers"
+import Schedule from "./Schedule"
 import Location from "./Location"
 import Partners from "./Partners"
-// import Sponsors from "./Sponsors"
+import Sponsors from "./Sponsors"
 // import Supporters from "./Supporters"
 import Footer from "../common/Footer"
 import EventDetails from "./EventDetails"
@@ -19,9 +19,9 @@ const HomePage = () => (
     <a href="#content" data-skip-content> Skip to content </a>
     <Main />
     <EventDetails />
-    {/* <Speakers /> */}
-    {/* <Schedule />
-    <Sponsors /> */}
+    <Speakers />
+    <Schedule />
+    {/* <Sponsors /> */}
     <Team />
     {/* <Supporters /> */}
     <Partners />
